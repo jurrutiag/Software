7e12be36d6b1873534c2c60e8de74963612653b3
Juan <juan.urrutia.gandolfo@gmail.com>
2017-05-10 23:49:42 -0300

    se modifico el archivo test_moverse ahora marca 1 y -1 la velocidad angular

--- catkin_ws/src/ros_cap/src/test_moverse.py
+++ catkin_ws/src/ros_cap/src/test_moverse.py
@@ -11,18 +11,22 @@ def main():
     rospy.init_node('test_publisher')
     rospy.loginfo('test_publisher')
     base_pub = rospy.Publisher('/duckiebot/wheels_driver_node/car_cmd', Twist2DStamped, queue_size=1)
-    P = rospy.Subscriber('/Point', Point, process_callback)
+    base_sub = rospy.Subscriber('/Point', Point, process_callback)
     msg = Twist2DStamped()
     rospy.spin()
 
 def process_callback(msg1):
     rospy.loginfo(msg1)
-    rospy.loginfo(msg1.axes[0])
+    rospy.loginfo(msg1.x)
     msg.header.stamp = rospy.get_rostime()
-    lados = msg1.axes[0]
-    vertical = msg1.axes[1]
-    msg.omega = lados*2
-    msg.v = vertical
+    X = msg1.x
+    if(X>325):
+         msg.omega = 1
+    elif(X<320 and X>0):
+         msg.omega = -1
+    else:
+         msg.omega = 0 
+    print msg.omega
     base_pub.publish(msg)
 
 if __name__ == '__main__':
